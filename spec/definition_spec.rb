a50094f4b33aff6fab483d233637c93e33fc4323
Matt Wilkin <mwilkin22@gmail.com>
2016-07-08 15:44:53 -0700

    add definition specs: 14 examples, 1 failure.

--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -8,4 +8,33 @@ describe(Definition) do
       expect(test_definition.definition).to(eq("The definition is something cool"))
     end
   end
+
+  describe('.all') do
+    it('starts out empty') do
+      expect(Definition.all()).to(eq([]))
+    end
+  end
+  describe('.clear') do
+    it('clears out all the saved definitions') do
+      Definition.new(:definition => 'foul smelling breath')
+      Definition.clear()
+      expect(Definition.all()).to(eq([]))
+    end
+  end
+  describe('#save') do
+    it('adds a definition to the saved definitions array') do
+      test_definition = Definition.new(:definition => "foul smelling breath")
+      test_definition.save()
+      expect(Definition.all()).to(eq([test_definition]))
+    end
+  end
+  describe('.find') do
+    it('returns a definition by its unique id number') do
+      test_definition = Definition.new(:definition => "foul smelling breath")
+      test_definition.save()
+      test_definition2 = Definition.new(:definition => "a stimulant")
+      test_definition2.save()
+      expect(Definition.find(test_definition.id())).to(eq(test_definition))
+    end
+  end
 end
